9b850c1c0809bb56604f008229f175cf2128d053
Kyohei Hamada <kyohei.hamada@gmail.com>
2015-08-04 13:03:39 +0900

    commit

--- main/main3.go
+++ main/main3.go
@@ -43,4 +43,87 @@ func main() {
 	// - 明示的に初期化しなかった場合に設定される値(型による)
 	var i int
 	fmt.Println(i) // => 0
+
+	// if
+	// {} は省略不可、三項演算子はナイ
+	a2, b2 := 10, 100
+	if a2 > b2 {
+		fmt.Println("a2 is lager than b2")
+	} else if a2 < b2 {
+		fmt.Println("a2 is smaller than b2")
+	} else {
+		fmt.Println("a2 equals b2")
+	}
+
+	// for
+	// while, loopはナイ
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// 無限ループ
+	// for {
+		// something()
+	// }
+	
+	// break, continue
+	n := 0
+	for {
+		n++
+		if n > 10 {
+			break // ループを抜ける
+		}
+		if n%2 ==0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+	func1()
+	func2()
+	func3()
+}
+func func1() {
+	n := 10
+	// go の switch は明示的にbreakする必要ナシ (ruby と一緒)
+	switch n {
+	case 15:
+		fmt.Println("FizzBuzz")
+	case 5,10:
+		fmt.Println("Buzz")
+	case 3,6,9:
+		fmt.Println("Fizz")
+	default:
+		fmt.Println(n)
+	}
+}
+func func2() {
+	n := 3
+	// 逆に次の処理を評価したいときに `fallthrough` を使う(ruby と一緒)
+	switch n {
+	case 3:
+		n = n - 1
+		fallthrough
+	case 2:
+		n = n - 1
+		fallthrough
+	case 1:
+		n = n - 1
+		fmt.Println(n) // => 0
+	}
+}
+
+func func3() {
+	n := 10
+	// caseには値だけじゃなくて式も指定できる
+	switch {
+	case n%15 == 0:
+		fmt.Println("FizzBuzz")
+	case n%5 == 0:
+		fmt.Println("Buzz")
+	case n%3 == 0:
+		fmt.Println("Fizz")
+	default:
+		fmt.Println(n)
+	}
 }
